51ea73875517298b190c38aa82c672e746b92617
Earl Adams <earladams2000@gmail.com>
2022-02-09 15:53:46 -0500

    setting up api

--- assets/styles.js
+++ assets/styles.js
@@ -1,25 +1,60 @@
 // weather app key
+//variables
 var APIKey = "a649ac03335b61b88b42edeea070ca64";
-var city;
-var queryURL = "api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
+var city = "";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
 //fetch(queryURL);
 
-async function getapi(queryURL) {
-    const reponse = await fetch(queryURL);
-
-    var data = await reponse.json();
-    console.log(data);
-    if (reponse) {
-        hideloader();
+function find(c) {
+    for (var i=0; i<sCity.length; i++) {
+        if(c.toUpperCase() === sCity[i]){
+            return -1;
+        }
     }
-    Show(data);
-}
-getapi(queryURL);
-
-function hideloader() {
-    document.getElementById('').style.display = 'none';
+    return 1;
 }
 
-function show(data) {
-    
+
+function weatherDisplay(event) {
+    event.preventDefault();
+    if(searchCity.val().trim()!=="") {
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
 }
+
+function currentWeather(city) {
+    queryURL;
+    $.ajax({
+        url:queryURL,
+        method:'GET',
+    }).then(function(reponse)
+ {
+     console.log(reponse);
+
+     var temp = (reponse.main.temp - 273.15) * 1.80 + 32;
+
+     $(currentTemperature).html((temp).toFixed(2)+"&#8457");
+
+     $(currentHumidty).html(reponse.main.humidity+"%");
+
+     var wind = reponse.wind.speed;
+     var windMPH = (wind*2.237).toFixed(1);
+     $(currentWSpeed).html(windMPH + "MPH");
+
+     
+
+
+ })
+}
+
+$('#search-button').on('click', weatherDisplay);
