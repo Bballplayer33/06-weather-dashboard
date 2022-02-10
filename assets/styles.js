3c2a9f06cf1311999611274289e33ea0c2793f8b
Earl Adams <earladams2000@gmail.com>
2022-02-10 11:53:53 -0500

    working product for current day

--- assets/styles.js
+++ assets/styles.js
@@ -5,15 +5,14 @@ var city = "";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
 var searchCity = $("#search-city");
 var searchButton = $("#search-button");
-var clearButton = $("#clear-history");
 var currentCity = $("#current-city");
 var currentTemperature = $("#temperature");
 var currentHumidty= $("#humidity");
 var currentWSpeed=$("#wind-speed");
-var currentUvindex= $("#uv-index");
 var sCity=[];
 //fetch(queryURL);
 
+// for loop for api
 function find(c) {
     for (var i=0; i<sCity.length; i++) {
         if(c.toUpperCase() === sCity[i]){
@@ -23,7 +22,7 @@ function find(c) {
     return 1;
 }
 
-
+//function to display weather
 function weatherDisplay(event) {
     event.preventDefault();
     if(searchCity.val().trim()!=="") {
@@ -33,7 +32,7 @@ function weatherDisplay(event) {
 }
 
 function currentWeather(city) {
-    queryURL;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
     $.ajax({
         url:queryURL,
         method:'GET',
@@ -43,18 +42,38 @@ function currentWeather(city) {
 
      var temp = (reponse.main.temp - 273.15) * 1.80 + 32;
 
-     $(currentTemperature).html((temp).toFixed(2)+"&#8457");
+     $(currentTemperature).html(( temp).toFixed(2)+"&#8457");
 
-     $(currentHumidty).html(reponse.main.humidity+"%");
+     $(currentHumidty).html( reponse.main.humidity+"%");
 
      var wind = reponse.wind.speed;
      var windMPH = (wind*2.237).toFixed(1);
-     $(currentWSpeed).html(windMPH + "MPH");
-
-     
-
-
- })
+     $(currentWSpeed).html( windMPH + "MPH");
+    })
 }
 
+function fiveDay(cityid) {
+    var dayover = false;
+    var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+    $.ajax({
+        url:fiveDayURL,
+        method:"GET"
+    }).then(function(response) {
+        console.log(response);
+        for (i=0;i<5;i++) {
+            var tempK = response.list[((i+1)*8)-1].main.temp;
+            var tempF = (((tempK - 273.5)*1.80)+32).toFixed(2);
+            var humidity = response.list[((i+1)*8)-1].main.humidity;
+
+
+            $("#fTemp"+i).html(tempF + "&#8457");
+            $("#fHumidity"+i).html(humidity + "%");
+        }
+    });
+}
+
+
+
+
 $('#search-button').on('click', weatherDisplay);
+$('#search-button').on('click', fiveDay);
